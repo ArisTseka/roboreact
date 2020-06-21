46c2a3e182815d4bd18672a6e77b0b08d0346179
aris <aristseka@gmail.com>
2020-06-21 18:33:27 +0300

    Updates

---
+++ precache-manifest.cf6d4656f9604de75c5eb55a25d83cc7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df9cc3263ab580c9f1a9ba8f3c9aa23a",
+    "url": "/roboreact/index.html"
+  },
+  {
+    "revision": "17672dfb047ce69c4862",
+    "url": "/roboreact/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "7f82be31821eb0f81501",
+    "url": "/roboreact/static/css/main.af23cfb8.chunk.css"
+  },
+  {
+    "revision": "17672dfb047ce69c4862",
+    "url": "/roboreact/static/js/2.e5b3756d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/roboreact/static/js/2.e5b3756d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f82be31821eb0f81501",
+    "url": "/roboreact/static/js/main.a3b4bc83.chunk.js"
+  },
+  {
+    "revision": "eb348b515306f035112f",
+    "url": "/roboreact/static/js/runtime-main.6147bcaa.js"
+  }
+]);
